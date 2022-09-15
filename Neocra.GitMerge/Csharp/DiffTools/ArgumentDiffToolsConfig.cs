3251bc516273f0541ae3c94e1ffcc5cd9548e06a
Aurélien GALTIER <1593976+swoog@users.noreply.github.com>
2022-09-15 14:55:15 +0200

    Fix sonar coverage

--- Neocra.GitMerge/Csharp/DiffTools/ArgumentDiffToolsConfig.cs
+++ Neocra.GitMerge/Csharp/DiffTools/ArgumentDiffToolsConfig.cs
@@ -7,31 +7,16 @@ namespace Neocra.GitMerge.Csharp.DiffTools;
 
 public class ArgumentDiffToolsConfig : DiffToolsConfig<ArgumentSyntax, ArgumentDiff>
 {
-    public override int Distance(ArgumentDiff delete, ArgumentDiff add)
-    {
-        throw new NotImplementedException();
-    }
-
     public override bool CanFusion(ArgumentDiff delete, ArgumentDiff add)
     {
         return false;
     }
-
-    public override ArgumentDiff CreateMove(ArgumentDiff delete, ArgumentDiff add)
-    {
-        throw new NotImplementedException();
-    }
-
+    
     public override bool IsElementEquals(ArgumentSyntax a, ArgumentSyntax b)
     {
-        return a.ToString() == b.ToString();
+        return a.ToFullString() == b.ToFullString();
     }
-        
-    public override Diff? MakeARecursive(ArgumentDiff delete, ArgumentDiff add)
-    {
-        throw new NotImplementedException();
-    }
-
+    
     public override ArgumentDiff CreateDiff(DiffMode mode, List<ArgumentSyntax> elements, int index)
     {
         return new ArgumentDiff(mode, index, 0, elements[index]);
